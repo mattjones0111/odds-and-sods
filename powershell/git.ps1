62650040f0e756c3d13c503198b5d53e00e6f4f0
Matthew Jones <matthew@funda.nl>
2020-10-30 13:56:52 +0100

    adds git script

--- powershell/git.ps1
+++ powershell/git.ps1
@@ -1,3 +1,7 @@
 # Delete all merged branches
 
 git branch --merged | %{$_.trim()} | ?{$_ -notmatch 'develop' -and $_ -notmatch 'master'} | %{git branch -d $_}
+
+# Pull all repos in subdirs
+
+Get-ChildItem -Directory | foreach { Write-Host ">>> Getting latest for $_" | git -C $_.FullName pull --all --recurse-submodules --verbose }
