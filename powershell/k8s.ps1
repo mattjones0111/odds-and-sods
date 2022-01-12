31ee5897066a2319e854f604f5185104e8741420
Matt Jones <matthew.jones@swapfiets.com>
2022-01-12 08:45:23 +0100

    add sql and k8s

---
+++ powershell/k8s.ps1
@@ -0,0 +1,3 @@
+# get and decode a secret
+
+kubectl get secret <<secretname>> -n <<namespace>> -o json | ConvertFrom-Json | ? data | select -ExpandProperty data | % { $_.PSObject.Properties | % { $_.Name + [System.Environment]::NewLine + [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($_.Value)) + [System.Environment]::NewLine + [System.Environment]::NewLine } }
--- sql/odds-and-sods.sql
+++ sql/odds-and-sods.sql
@@ -55,3 +55,15 @@ SELECT [CurrentStep],
                (([ElapsedSeconds] / [RowsProcessed]) * [RowsLeft]),
             GETDATE()) AS [EstimatedCompletionTime]
 FROM   comp;
+
+-- show db users and roles
+
+SELECT isnull (DP2.name, 'No members') AS [User],
+    DP1.name AS [Role]
+FROM sys.database_role_members AS DRM  
+RIGHT OUTER JOIN sys.database_principals AS DP1  
+    ON DRM.role_principal_id = DP1.principal_id  
+LEFT OUTER JOIN sys.database_principals AS DP2  
+    ON DRM.member_principal_id = DP2.principal_id  
+WHERE DP1.type = 'R'
+ORDER BY DP2.name;  
