1f4b9725821b35a2f7b4c131e3ec809df0663b61
Matt Jones <m.jones@fourthline.com>
2023-08-16 08:09:21 +0200

    update docker run for azurite

--- docker/start-azurite.ps1
+++ docker/start-azurite.ps1
@@ -1,4 +1,4 @@
 # pull the azurite image and start a new container with it
 # connection strings = https://docs.microsoft.com/en-us/azure/storage/common/storage-use-azurite
 
-docker run -p 10000:10000 -p 10001:10001 mcr.microsoft.com/azure-storage/azurite
+docker run -p 10000:10000 -p 10001:10001 -p 10002:10002 mcr.microsoft.com/azure-storage/azurite
